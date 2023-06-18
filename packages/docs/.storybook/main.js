b287147a66a99968f468ced1ef18b8ae2af637b3
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2023-06-18 16:20:45 -0300

    Add docs workflow

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -9,13 +9,20 @@ const config = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-dark-mode",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+  core: {
+    builder: "@storybook/builder-vite",
+  },
   docs: {
     autodocs: "tag",
+  },
+  features: {
+    storyStoreV7: true,
   },
 };
 export default config;
